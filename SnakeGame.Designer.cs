f2c9835ead4bcb5bb49825b2d66695ad0432fafc
Md-Umar02 <mdumar71102@gmail.com>
2025-02-03 03:59:41 +0530

    First Version of Snake Game

--- SnakeGame.Designer.cs
+++ SnakeGame.Designer.cs
@@ -41,7 +41,7 @@
 			// 
 			this.label1.AutoSize = true;
 			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(687, 21);
+			this.label1.Location = new System.Drawing.Point(12, 398);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(79, 29);
 			this.label1.TabIndex = 0;
@@ -53,7 +53,7 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
 			this.Controls.Add(this.label1);
-			this.Name = "Snake Game";
+			this.Name = "SnakeGame";
 			this.Text = "Snake Game";
 			this.ResumeLayout(false);
 			this.PerformLayout();
